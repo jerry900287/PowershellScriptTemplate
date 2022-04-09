5a4932df23c08c50f8974082f6083d1089548c36
Jerry <jerry900287@gmail.com>
2022-04-09 11:49:48 +0800

    Enhance Template

--- PowershellScriptTemplate.ps1
+++ PowershellScriptTemplate.ps1
@@ -1,17 +1,4 @@
-﻿#Initial Setting
-$fileName = "Test"
-$logFolder = "D:\Testing"
-$logFile = "$(Get-Date -f yyyyMMdd-HHmmss)_$($fileName).log" 
-$logReservedQuantity = 5
-
-if (!(Test-Path $logFolder)){
-    New-Item -ItemType directory -Path $logFolder
-}
-
-Write-Log "Hi"
-
-Delete-File -Path $logFolder -ReservedQuantity $logReservedQuantity
-
+﻿#==== Function for writing log ====
 Function Write-Log {
     param (
         [string]$logString = "",
@@ -20,12 +7,41 @@ Function Write-Log {
     Write-Host "$(Get-Date -f 'yyyy-MM-dd HH:mm:ss') | $($logString)" 
     Write-Output "$(Get-Date -f 'yyyy-MM-dd HH:mm:ss') | $($logString)" | Out-File -FilePath $logPath -Append
 }
+#===================================
+
+#==== Function for removing item ====
 Function Delete-File{
     param (
         [string]$path = "$($logFolder)",
-        [int]$reservedQuantity = $logReservedQuantity
+        [int]$reservedQuantity = 30,
+        [string]$mode = "Count"
     )
     
-    $reservedFiles = Get-ChildItem -Path $logFolder | Select-Object -ExcludeProperty Name -last $reservedQuantity
-    Get-ChildItem -Path $logFolder -Exclude $reservedFiles | Remove-Item -Force -ErrorAction SilentlyContinue
+    if ($mode -eq "Count"){
+        $reservedFiles = Get-ChildItem -Path $path | Select-Object -ExpandProperty Name -last $reservedQuantity
+        Get-ChildItem -Path $path -Exclude $reservedFiles | Remove-Item -Force -ErrorAction SilentlyContinue
+    }
+
+    if ($mode -eq "Day"){
+        $reservedFiles = Get-ChildItem -Path $path | Where-Object {$_.LastWriteTime -lt $(Get-Date).AddDays(-($reservedQuantity))}
+        $reservedFiles | Remove-Item -Force -ErrorAction SilentlyContinue
+    }
 }
+#===================================
+
+#==== Initial Log Setting ====
+$projectName = "Test"
+$logFolder = "D:\Testing"
+$logFile = "$(Get-Date -f yyyyMMdd-HHmmss)_$($projectName).log" 
+$logReservedQuantity = 5
+$logReservedMode = "Count"
+
+if (!(Test-Path $logFolder)){
+    New-Item -ItemType directory -Path $logFolder
+}
+
+Delete-File -Path $logFolder -ReservedQuantity $logReservedQuantity -Mode $logReservedMode
+#===================================
+
+Write-Log "Hello World"
+
